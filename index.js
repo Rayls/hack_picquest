50da5ae74b674d98e5e972d9e5ea3dbd6e06bd8c
Dawson Reid <dreid93@gmail.com>
2013-09-10 01:48:56 -0300

    Added content to index.js.

---
+++ index.js
@@ -0,0 +1,66 @@
+/*
+Dawson Reid
+Glavin Wiechert
+
+A basic index.js template for Node.js projects using socket.io and
+express.
+*/
+
+var 
+  http = require('http'),
+  util = require('util'),
+  fs = require('fs'),
+  url = require('url'),
+  express = require('express');
+
+var userStore = {};
+
+// create and init my server
+var 
+    app = express(),
+    server = http.createServer(app),
+    memStore = new express.session.MemoryStore();
+
+console.log('Configuring server.');
+
+app.configure(function(){
+  
+  // web server config
+  app.use(express.methodOverride());
+  app.use(express.bodyParser());
+  app.use(express.static(__dirname));
+  
+  app.use(express.errorHandler({
+    dumpExceptions: true, 
+    showStack: true
+  }));
+  
+  // session support
+  app.use(express.cookieParser());
+  app.use(express.session({
+        store: memStore,
+        secret: 'dugtrio',
+        key: 'diglett.sid'
+    }));
+    
+    app.use(app.router);
+});
+
+console.log('Starting server.');
+
+/*
+Here is where all of the app.get(... code goes. 
+*/
+
+app.get('/', function(req, resp) {
+  resp.sendfile('site/index.html');
+});
+
+server.listen(1337);
+
+io = require('socket.io').listen(server);
+io.sockets.on('connection', function (socket) {
+    
+    console.log('Socket connection.');
+
+});
